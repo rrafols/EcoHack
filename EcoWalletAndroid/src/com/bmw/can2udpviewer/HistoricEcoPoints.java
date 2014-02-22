58f52140318414f82a8b88362c0ff8f5943f8c84
tisi1988 <tisi1988@gmail.com>
2014-02-22 23:34:18 +0100

    Update UI

---
+++ EcoWalletAndroid/src/com/bmw/can2udpviewer/HistoricEcoPoints.java
@@ -0,0 +1,15 @@
+package com.bmw.can2udpviewer;
+
+import com.hacktheride.ecowallet.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HistoricEcoPoints extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historic);
+    }
+}
