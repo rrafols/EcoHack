43c9e3489e9394ef1c2352598a286852f9e07db9
tisi1988 <tisi1988@gmail.com>
2014-02-23 00:40:36 +0100

    Historic List

--- EcoWalletAndroid/src/com/bmw/can2udpviewer/MainActivity.java
+++ EcoWalletAndroid/src/com/bmw/can2udpviewer/MainActivity.java
@@ -1,41 +1,79 @@
 package com.bmw.can2udpviewer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.hacktheride.ecowallet.R;
 
-
-public class MainActivity extends Activity implements EventCallbackUDP, EcoPointsChangeListener {
+public class MainActivity extends Activity implements EventCallbackUDP,
+		EcoPointsChangeListener {
 	public static final String TAG = "HackTheRide";
-	
+
 	private UDPReceiver udpReceiver = new UDPReceiver(30002, this);
 	private EcoPointsChangeListener ecoPointsListener;
 	private int lastEcoPoints = 0;
 	private int accEcoPoints = 0;
-	
-	//zaska
+	private ArrayList<Integer> mHistoricEcoPointsValues = new ArrayList<Integer>();
+	private ArrayList<String> mHistoricEcoPointsDates = new ArrayList<String>();
+	private Activity mThisActivity;
+
+	// zaska
 	public static UDPData udpData = new UDPData();
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        udpReceiver.start();
-    }
-    
-    public void setEcoPointsChangeListener(EcoPointsChangeListener listener) {
-    	this.ecoPointsListener = listener;
-    }
-    
-    @Override
-    protected void onDestroy() {
-    	udpReceiver.Terminate();
-    	super.onDestroy();
-    }
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		udpReceiver.start();
+
+		// Hardcoded historic EcoPoints
+		mHistoricEcoPointsValues.add(Integer.valueOf(5));
+		mHistoricEcoPointsDates.add("02/12/2014 15:00");
+		mHistoricEcoPointsValues.add(Integer.valueOf(2));
+		mHistoricEcoPointsDates.add("02/12/2014 17:43");
+		mHistoricEcoPointsValues.add(Integer.valueOf(3));
+		mHistoricEcoPointsDates.add("02/13/2014 09:03");
+		mHistoricEcoPointsValues.add(Integer.valueOf(0));
+		mHistoricEcoPointsDates.add("02/14/2014 14:12");
+		mHistoricEcoPointsValues.add(Integer.valueOf(1));
+		mHistoricEcoPointsDates.add("02/14/2014 15:00");
+
+		mThisActivity = this;
+		
+		// Historic button listener
+		Button historic = (Button) findViewById(R.id.botonHistorico);
+		historic.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(mThisActivity, HistoricEcoPoints.class);
+				intent.putIntegerArrayListExtra("HistoricPointsValue", mHistoricEcoPointsValues);
+				intent.putStringArrayListExtra("HistoricPointsDates", mHistoricEcoPointsDates);
+				mThisActivity.startActivity(intent);
+			}
+		});
+	}
+
+	public void setEcoPointsChangeListener(EcoPointsChangeListener listener) {
+		this.ecoPointsListener = listener;
+	}
+
+	@Override
+	protected void onDestroy() {
+		udpReceiver.Terminate();
+		super.onDestroy();
+	}
 
 	@Override
 	public void EventReceivedUDP(UDPData udpData) {
@@ -43,34 +81,49 @@ public class MainActivity extends Activity implements EventCallbackUDP, EcoPoint
 		double energyDischargeFactor = 0.00015259;
 		double consumptionWeight = 0.5;
 		double rechargeWeight = 0.5;
-		
-		double discharge = ((double) udpData.EnergyDischarged.getValue()) * energyDischargeFactor;
-		double recharge = ((double) udpData.EnergyChargedByRecuperation.getValue()) * energyRechargeFactor;
-		
+
+		double discharge = ((double) udpData.EnergyDischarged.getValue())
+				* energyDischargeFactor;
+		double recharge = ((double) udpData.EnergyChargedByRecuperation
+				.getValue()) * energyRechargeFactor;
+
 		double dischargePoints = 0;
-		if (discharge >= 8.0) dischargePoints = 1;
-		else if (discharge >= 7.0) dischargePoints = 2;
-		else if (discharge >= 6.0) dischargePoints = 3;
-		else if (discharge >= 5.0) dischargePoints = 4;
-		else dischargePoints = 5;
-		
+		if (discharge >= 8.0)
+			dischargePoints = 1;
+		else if (discharge >= 7.0)
+			dischargePoints = 2;
+		else if (discharge >= 6.0)
+			dischargePoints = 3;
+		else if (discharge >= 5.0)
+			dischargePoints = 4;
+		else
+			dischargePoints = 5;
+
 		double rechargePoints = 0;
-		if (recharge <= 0.5) rechargePoints = 1;
-		else if (recharge <= 1.0) rechargePoints = 2;
-		else if (recharge <= 1.5) rechargePoints = 3;
-		else if (recharge <= 2.0) rechargePoints = 4;
-		else rechargePoints = 5;
-		
-		double ecoPointsF = (consumptionWeight * dischargePoints + rechargeWeight * rechargePoints) / 2.0;
+		if (recharge <= 0.5)
+			rechargePoints = 1;
+		else if (recharge <= 1.0)
+			rechargePoints = 2;
+		else if (recharge <= 1.5)
+			rechargePoints = 3;
+		else if (recharge <= 2.0)
+			rechargePoints = 4;
+		else
+			rechargePoints = 5;
+
+		double ecoPointsF = (consumptionWeight * dischargePoints + rechargeWeight
+				* rechargePoints) / 2.0;
 		long ecoPointsR = Math.round(ecoPointsF);
-		Log.i(TAG, "EcoPoints: " + discharge + " (" + dischargePoints + "), " + recharge + " (" + rechargePoints + ") " + ecoPointsF + "(" + ecoPointsR + ") vs " + udpData.ECOPoints.getValue());
-		
+		Log.i(TAG, "EcoPoints: " + discharge + " (" + dischargePoints + "), "
+				+ recharge + " (" + rechargePoints + ") " + ecoPointsF + "("
+				+ ecoPointsR + ") vs " + udpData.ECOPoints.getValue());
+
 		int ecoPoints = udpData.ECOPoints.getValue();
-		if(ecoPoints != lastEcoPoints) {
+		if (ecoPoints != lastEcoPoints) {
 			int delta = ecoPoints - ecoPoints;
 			accEcoPoints += delta;
-			
-			if(ecoPointsListener != null) {
+
+			if (ecoPointsListener != null) {
 				ecoPointsListener.ecoPointsChange(delta);
 			}
 		}
@@ -79,7 +132,7 @@ public class MainActivity extends Activity implements EventCallbackUDP, EcoPoint
 	@Override
 	public void sessionStarted() {
 		lastEcoPoints = -1;
-		
+
 		if (ecoPointsListener != null) {
 			ecoPointsListener.sessionStarted();
 		}
@@ -88,31 +141,36 @@ public class MainActivity extends Activity implements EventCallbackUDP, EcoPoint
 	@Override
 	public void sessionStopped() {
 		if (ecoPointsListener != null) {
-			ecoPointsListener.sessionStopped((accEcoPoints > 0) ? accEcoPoints : 0);
+			ecoPointsListener.sessionStopped((accEcoPoints > 0) ? accEcoPoints
+					: 0);
 		}
 	}
 
 	@Override
 	public void sessionStopped(int sessionTotal) {
 		// TODO Comunicar al servidor
-		
+
+		mHistoricEcoPointsValues.add(Integer.valueOf(sessionTotal));
+
 		// UI global ecoPoints score
 		TextView totalEcopints = (TextView) findViewById(R.id.totalPoints);
 		int lastTotalScore = Integer.parseInt((String) totalEcopints.getText());
 		totalEcopints.setText(Integer.toString(lastTotalScore + sessionTotal));
-		
+
 		// UI last ride update
 		TextView lastRide = (TextView) findViewById(R.id.lastRidePunctuations);
-		if(sessionTotal == 0)
-			lastRide.setTextColor(getResources().getColor(android.R.color.holo_orange_light));
+		if (sessionTotal == 0)
+			lastRide.setTextColor(getResources().getColor(
+					android.R.color.holo_orange_light));
 		else
-			lastRide.setTextColor(getResources().getColor(android.R.color.holo_green_light));
+			lastRide.setTextColor(getResources().getColor(
+					android.R.color.holo_green_light));
 		lastRide.setText(Integer.toString(sessionTotal));
 	}
 
 	@Override
 	public void ecoPointsChange(int delta) {
 		// TODO Auto-generated method stub
-		// TODO Modificar la UI		
-	}    
+		// TODO Modificar la UI
+	}
 }
